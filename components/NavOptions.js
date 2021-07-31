1f54fcf8be071e8454c43deb95fa4ec16ff85fad
Siddhesh Kankekar <kankekas@tcd.ie>
2021-07-31 22:27:41 +0100

    implemented React Native Navigation

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, Image, TouchableOpacity, FlatList } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const data = [
   {
@@ -19,13 +20,18 @@ const data = [
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
+
   return (
     <FlatList
       data={data}
       horizontal
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
           <View>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
@@ -34,7 +40,7 @@ const NavOptions = () => {
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
             <Icon
               style={tw`p-2 bg-black rounded-full w-10 mt-4`}
-              type="arrowright"
+              name="arrowright"
               color="white"
               type="antdesign"
             />
